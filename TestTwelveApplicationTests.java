1e6740647dd7856e38b2df98b2b5ca9fc526d8b1
TharushiNK <s23010921@ousl.lk>
2025-02-25 22:01:51 +0530

    Add files via upload
    IntelliJ IDEA
    MYSQL Wokrbench

---
+++ TestTwelveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_twelve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTwelveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
